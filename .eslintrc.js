3d1133853247644b2e4b00cdc3969e950ce4da05
ifmoon <ifmoon.io@gmail.com>
2020-10-07 02:00:48 +0900

    add eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
+    'max-depth': ['error', { max: 3 }],
+    'no-unneeded-ternary': ['error', { defaultAssignment: false }],
+    'simple-import-sort/sort': 'error',
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never', json: 'never' },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+  },
+  env: {
+    jest: true,
+    node: true,
+    browser: true,
+  },
+  settings: {
+    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] } },
+  },
+};
