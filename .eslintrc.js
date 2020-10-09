cb45fbf23d168139029282a40fff10b04b70662c
ifmoon <ifmoon.io@gmail.com>
2020-10-10 01:02:30 +0900

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
-  plugins: ['import','simple-import-sort'],
+  extends: ['airbnb-typescript', 'plugin:prettier/recommended'],
+  plugins: ['simple-import-sort'],
   rules: {
     'padding-line-between-statements': [
       'error',
@@ -12,29 +11,11 @@ module.exports = {
     'simple-import-sort/sort': 'error',
     'sort-imports': 'off',
     'import/order': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      { js: 'never', jsx: 'never', ts: 'never', tsx: 'never', json: 'never' },
-    ],
-    'react/jsx-filename-extension': [
-      2,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    'import/no-unresolved': "error"
+  },
+  parserOptions: {
+    project: './tsconfig.json',
   },
   env: {
-    jest: true,
-    node: true,
-    browser: true,
-  },
-  settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
-    },
-    'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },  
-    "typescript": {
-      "alwaysTryTypes": true 
-    }},
-  },
+    browser:true
+  }
 };
